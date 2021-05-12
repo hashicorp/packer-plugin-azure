2175e71bde74806654d8cb39633027be125dce64
Sylvia Moss <moss@hashicorp.com>
2021-05-12 21:35:07 +0200

    Revert version changes and fix goreleaser config (#84)
    * Revert "fix plugin version (#75)"
    
    This reverts commit 658cead4d50cae24d74888dde07b797cd03a6017.
    
    * Fix goreleaser ldflags to ser version
    
    * Bump to next dev version
    
    * Add a simple test for validating version info
    
    Co-authored-by: Wilken Rivera <dev@wilkenrivera.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,11 @@
-## 0.0.1 (Unreleased)
+## 0.0.2 (Unreleased)
+
+BUG FIXES:
+
+* Fixes a version string issue to support plugin vendoring from within Packer [hashicorp/packer#10979](https://github.com/hashicorp/packer/pull/10979).
+  [GH-84]
+
+## 0.0.1 (May 7, 2021)
 
 * Azure Plugin break out from Packer core. Changes prior to break out can be found in [Packer's CHANGELOG](https://github.com/hashicorp/packer/blob/master/CHANGELOG.md)
 
---
+++ version/version_test.go
@@ -0,0 +1,19 @@
+package version
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAzurePluginVersion_FormattedVersion(t *testing.T) {
+	if AzurePluginVersion == nil {
+		t.Fatal("Unable to continue with nil version")
+	}
+
+	expected := fmt.Sprintf("%s-%s", Version, VersionPrerelease)
+	got := AzurePluginVersion.FormattedVersion()
+	if got != expected {
+		t.Errorf("calling FormattedVersion on AzurePluginVersion failed: expected %s, but got %s", expected, got)
+	}
+
+}
