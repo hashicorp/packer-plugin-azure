c8406f4b060155e46b9cc2bb96dc73f17d2cfaf7
Wilken Rivera <dev@wilkenrivera.com>
2021-05-12 16:44:13 -0400

    version_test: Update to work with non-prerelease version strings

--- version/version_test.go
+++ version/version_test.go
@@ -10,7 +10,10 @@ func TestAzurePluginVersion_FormattedVersion(t *testing.T) {
 		t.Fatal("Unable to continue with nil version")
 	}
 
-	expected := fmt.Sprintf("%s-%s", Version, VersionPrerelease)
+	expected := Version
+	if VersionPrerelease != "" {
+		expected = fmt.Sprintf("%s-%s", Version, VersionPrerelease)
+	}
 	got := AzurePluginVersion.FormattedVersion()
 	if got != expected {
 		t.Errorf("calling FormattedVersion on AzurePluginVersion failed: expected %s, but got %s", expected, got)
