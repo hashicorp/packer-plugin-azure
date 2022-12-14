abf582868162c2c67afd0c0f9775a1e5104f6545
Matt Richardson <matt.richardson@octopus.com>
2022-12-14 11:43:23 +1100

    Update template_factory.go

--- builder/azure/arm/template_factory.go
+++ builder/azure/arm/template_factory.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2021-11-01/compute"
 	"github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2018-02-01/resources"
+
 	"github.com/hashicorp/packer-plugin-azure/builder/azure/common/constants"
 	"github.com/hashicorp/packer-plugin-azure/builder/azure/common/template"
 	"golang.org/x/crypto/ssh"
