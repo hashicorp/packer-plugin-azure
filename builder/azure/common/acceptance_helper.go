cadad3eec7e6fb237b763384b2ee104e2c6048a9
Jenna Goldstrich <jenna.goldstrich@hashicorp.com>
2023-11-15 17:48:12 -0800

    Remove unneeded returns

--- builder/azure/common/acceptance_helper.go
+++ builder/azure/common/acceptance_helper.go
@@ -14,33 +14,25 @@ type CheckAcceptanceTestEnvVarsParams struct {
 func CheckAcceptanceTestEnvVars(t *testing.T, params CheckAcceptanceTestEnvVarsParams) {
 	if os.Getenv("PACKER_ACC") == "" {
 		t.Skipf("Skipping acceptance test %s has environment variable `PACKER_ACC` is not set", t.Name())
-		return
 	}
-
 	if os.Getenv("ARM_RESOURCE_GROUP_NAME") == "" {
 		t.Fatalf("Test %s requires environment variable ARM_RESOURCE_GROUP_NAME is set", t.Name())
-		return
 	}
 	if os.Getenv("ARM_CLIENT_ID") == "" {
 		t.Fatalf("Test %s requires environment variable ARM_CLIENT_ID is set", t.Name())
-		return
 	}
 	if os.Getenv("ARM_CLIENT_SECRET") == "" {
 		t.Fatalf("Test %s requires environment variable ARM_CLIENT_SECRET is set", t.Name())
-		return
 	}
 	if os.Getenv("ARM_SUBSCRIPTION_ID") == "" {
 		t.Fatalf("Test %s requires environment variable ARM_SUBSCRIPTION_ID is set", t.Name())
-		return
 	}
 
 	if params.CheckAzureCLI && !loggedIntoAzureCLI(t) {
 		t.Fatalf("Test %s requires CLI authentication, install the Azure CLI and log in", t.Name())
-		return
 	}
 	if params.CheckSSHPrivateKeyFile && os.Getenv("ARM_SSH_PRIVATE_KEY_FILE") == "" {
 		t.Fatalf("Test %s requires environment variable ARM_SSH_PRIVATE_KEY_FILE is set", t.Name())
-		return
 	}
 }
 
