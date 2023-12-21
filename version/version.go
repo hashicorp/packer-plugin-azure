452c11e855bda4b1625f5d1c7c60165759756e7a
Lucas Bajolet <lucas.bajolet@hashicorp.com>
2023-12-21 14:26:12 -0500

    version: prepare release 2.0.2

--- version/version.go
+++ version/version.go
@@ -9,12 +9,12 @@ import (
 
 var (
 	// Version is the main version number that is being run at the moment.
-	Version = "2.0.1"
+	Version = "2.0.2"
 
 	// VersionPrerelease is A pre-release marker for the Version. If this is ""
 	// (empty string) then it means that it is a final release. Otherwise, this
 	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = "dev"
+	VersionPrerelease = ""
 
 	// AzurePluginVersion is used by the plugin set to allow Packer to recognize
 	// what version this plugin is.
