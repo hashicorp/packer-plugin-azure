88f27eac17aa293ee3ecae2f02fbcdc27aa93711
Lucas Bajolet <lucas.bajolet@hashicorp.com>
2024-01-23 16:00:09 -0500

    log: add mini log library
    The Azure log library mimicks what the standard log library allows (with
    fewer functions exposed), and uses what the Packer SDK exposes for
    filtering out sensitive strings.
    
    We have to do this here for censoring potential sensitive data
    separately from Packer itself, as the secrets produced/consumed by the
    plugin itself aren't necessarily known by Packer, so it cannot censor
    them from its output, and we have no way for a plugin to declare what is
    sensitive in its logs.
    
    Therefore, we do it at log-time, in the plugin process, so Packer
    receives pre-censored logs, which it can render normally through its
    pipeline.

---
+++ builder/azure/common/log/log.go
@@ -0,0 +1,28 @@
+// log allows users to replicate Packer's behaviour with logging, i.e. mask
+// potential secrets by replacing them if they occur with `<sensitive>`.
+//
+// This is intended as a drop-in replacement for the standard `log` package,
+// and relies on it for final printing.
+package log
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/packer-plugin-sdk/packer"
+)
+
+func Print(v ...any) {
+	raw := string(fmt.Append(nil, v...))
+	log.Print(packer.LogSecretFilter.FilterString(raw))
+}
+
+func Printf(format string, v ...any) {
+	raw := string(fmt.Appendf(nil, format, v...))
+	log.Print(packer.LogSecretFilter.FilterString(raw))
+}
+
+func Println(v ...any) {
+	raw := string(fmt.Appendln(nil, v...))
+	log.Print(packer.LogSecretFilter.FilterString(raw))
+}
