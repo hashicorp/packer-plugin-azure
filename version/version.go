e12e55563ecf1ffba1d67a4e856d47b5854849dc
Jenna Goldstrich <jenna.goldstrich@hashicorp.com>
2025-02-24 11:02:12 -0800

    Cut v2.3.0

--- version/version.go
+++ version/version.go
@@ -8,7 +8,7 @@ import (
 )
 
 var (
-	Version            = "2.2.1"
+	Version            = "2.3.0"
 	VersionPrerelease  = ""
 	VersionMetadata    = ""
 	AzurePluginVersion = version.NewPluginVersion(Version, VersionPrerelease, VersionMetadata)
