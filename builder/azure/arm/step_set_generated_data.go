cf804ca4fab4c6fe1fe4782ba965518a61dc77c2
Jenna Goldstrich <jenna.goldstrich2@gmail.com>
2025-04-09 07:12:36 -0700

    Add temp resource names to Generated Data (#488)
    * Add temp resource names to generated data: https://github.com/hashicorp/packer-plugin-azure/issues/369
    
    * Docs

--- builder/azure/arm/step_set_generated_data.go
+++ builder/azure/arm/step_set_generated_data.go
@@ -19,6 +19,17 @@ func (s *StepSetGeneratedData) Run(ctx context.Context, state multistep.StateBag
 
 	s.GeneratedData.Put("TenantID", s.Config.ClientConfig.TenantID)
 	s.GeneratedData.Put("SubscriptionID", s.Config.ClientConfig.SubscriptionID)
+	s.GeneratedData.Put("TempComputeName", s.Config.tmpComputeName)
+	s.GeneratedData.Put("TempNicName", s.Config.tmpNicName)
+	s.GeneratedData.Put("TempOSDiskName", s.Config.tmpOSDiskName)
+	s.GeneratedData.Put("TempDataDiskName", s.Config.tmpDataDiskName)
+	s.GeneratedData.Put("TempDeploymentName", s.Config.tmpDeploymentName)
+	s.GeneratedData.Put("TempVirtualNetworkName", s.Config.tmpVirtualNetworkName)
+	s.GeneratedData.Put("TempKeyVaultName", s.Config.tmpKeyVaultName)
+	s.GeneratedData.Put("TempResourceGroupName", s.Config.tmpResourceGroupName)
+	s.GeneratedData.Put("TempNsgName", s.Config.tmpNsgName)
+	s.GeneratedData.Put("TempSubnetName", s.Config.tmpSubnetName)
+	s.GeneratedData.Put("TempPublicIPAddressName", s.Config.tmpPublicIPAddressName)
 	return multistep.ActionContinue
 }
 
