0b1d2f188a582a46735c99fea1d46f21bde728e6
Tanmay Jain <tanmay.jain@hashicorp.com>
2026-05-18 18:06:53 +0530

    fix(winrm): default to NTLM auth using Trilean, allow opt-out with winrm_use_ntlm=false

--- builder/azure/dtl/config_test.go
+++ builder/azure/dtl/config_test.go
@@ -207,7 +207,7 @@ func TestWinRMConfigShouldSetRoundTripDecoratorWhenNTLMEnabled(t *testing.T) {
 	}
 }
 
-func TestWinRMConfigShouldNotSetRoundTripDecoratorWhenNTLMDisabled(t *testing.T) {
+func TestWinRMConfigShouldSetRoundTripDecoratorWhenNTLMUnset(t *testing.T) {
 	config_dtl := getDtlBuilderConfiguration()
 	config_dtl["communicator"] = "winrm"
 	config_dtl["winrm_username"] = "username"
@@ -219,8 +219,26 @@ func TestWinRMConfigShouldNotSetRoundTripDecoratorWhenNTLMDisabled(t *testing.T)
 		t.Errorf("Expected configuration creation to succeed, but it failed (%s)!\n", err)
 	}
 
+	if config.Comm.WinRMTransportDecorator == nil {
+		t.Error("Expected WinRMTransportDecorator to be set when winrm_use_ntlm is unset (Azure defaults to NTLM), but it was nil")
+	}
+}
+
+func TestWinRMConfigShouldNotSetRoundTripDecoratorWhenNTLMDisabled(t *testing.T) {
+	config_dtl := getDtlBuilderConfiguration()
+	config_dtl["communicator"] = "winrm"
+	config_dtl["winrm_username"] = "username"
+	config_dtl["winrm_password"] = "password"
+	config_dtl["winrm_use_ntlm"] = "false"
+
+	config := Config{}
+	_, err := config.Prepare(config_dtl, getPackerConfiguration())
+	if err != nil {
+		t.Errorf("Expected configuration creation to succeed, but it failed (%s)!\n", err)
+	}
+
 	if config.Comm.WinRMTransportDecorator != nil {
-		t.Error("Expected WinRMTransportDecorator to be nil when winrm_use_ntlm is not set, but it was set")
+		t.Error("Expected WinRMTransportDecorator to be nil when winrm_use_ntlm is false, but it was set")
 	}
 }
 
