e2bb7c22f923292d4299e087b08f7bde82375fb7
Thomas Meckel <tmeckel@users.noreply.github.com>
2026-05-31 15:13:18 +0000

    feat(terraform): add vnet, subnet and resource outputs
    * Add azurerm_virtual_network and azurerm_subnet resources to support network security group testing in acceptance tests.
    * Introduce outputs.tf to expose created resource names for debugging and CI verification. This helps confirm that acceptance tests reference the correct resources and simplifies local test setup.
    * Update README to document the new outputs and fix minor formatting.

--- terraform/main.tf
+++ terraform/main.tf
@@ -98,6 +98,20 @@ resource "azurerm_key_vault" "vault" {
   }
 }
 
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${local.resource_prefix}_vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
 /*
 ## DTL Builder Resources - disabled
 
