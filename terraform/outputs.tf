b546a972afad00273b0ae6103002e50c902aeb1e
Thomas Meckel <tmeckel@users.noreply.github.com>
2026-06-02 20:55:53 +0000

    feat(terraform): add subnet name output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -24,3 +24,7 @@ output "resource_suffix" {
 output "virtual_network_name" {
   value = azurerm_virtual_network.vnet.name
 }
+
+output "virtual_network_subnet_name" {
+  value = azurerm_subnet.subnet.name
+}
