e09549528bddcf25159c689d3a457c71d7789476
Hari Om <hari.om2@ibm.com>
2026-09-18 16:32:45 +0530

    release: bump version to 2.6.5-dev

--- version/version.go
+++ version/version.go
@@ -8,8 +8,8 @@ import (
 )
 
 var (
-	Version            = "2.6.4"
-	VersionPrerelease  = ""
+	Version            = "2.6.5"
+	VersionPrerelease  = "dev"
 	VersionMetadata    = ""
 	AzurePluginVersion = version.NewPluginVersion(Version, VersionPrerelease, VersionMetadata)
 )
